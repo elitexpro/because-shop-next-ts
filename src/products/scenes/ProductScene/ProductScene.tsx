cdb607cfbbcb55bd773082322340fe76db32bba1
elitexpro <cryptooxelite@gmail.com>
2023-03-13 11:42:59 -0500

    [ADD]: product scene + slideshow dep

---
+++ src/products/scenes/ProductScene/ProductScene.tsx
@@ -0,0 +1,7 @@
+interface ProductSceneProps {}
+
+const ProductScene: React.FC<ProductSceneProps> = () => {
+  return <div>ProductScene</div>;
+};
+
+export default ProductScene;
--- src/themes/light-theme.ts
+++ src/themes/light-theme.ts
@@ -10,6 +10,8 @@ export const lightTheme = createTheme({
     secondary: {
       main: '#3A64D8',
     },
+
+
     info: {
       main: '#fff',
     },
