b49ff8e0dbbf213bb58a82858662b05ca33ff63a
elitexpro <cryptooxelite@gmail.com>
2023-03-13 11:01:04 -0500

    [ADD]: ProductCard component design

--- src/products/scenes/HomeScene/components/ProductCard/ProductCard.tsx
+++ src/products/scenes/HomeScene/components/ProductCard/ProductCard.tsx
@@ -1,23 +1,54 @@
-import { Grid, Card, CardActionArea, CardMedia } from '@mui/material';
+import {
+  Grid,
+  Card,
+  CardActionArea,
+  CardMedia,
+  Box,
+  Typography,
+} from '@mui/material';
 
 import { IProduct } from '@/interfaces';
+import { useState } from 'react';
 
 interface ProductCardProps {
   product: IProduct;
 }
 
+interface PCState {
+  isHovered: boolean;
+}
+
 const ProductCard: React.FC<ProductCardProps> = ({ product }) => {
+  const [isHovered, setIsHovered] = useState<PCState['isHovered']>(false);
+
   return (
-    <Grid item key={product.slug} xs={6} sm={4}>
+    <Grid
+      item
+      xs={6}
+      sm={4}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
       <Card>
         <CardActionArea>
           <CardMedia
             component="img"
-            image={`products/${product.images[0]}`}
+            image={
+              isHovered
+                ? `products/${product.images[1]}`
+                : `products/${product.images[0]}`
+            }
             alt={product.title}
+            className="fadeIn"
+            // onLoad={() => console.log('loaded')}
           />
         </CardActionArea>
       </Card>
+
+      <Box sx={{ mt: 1 }} className="fadeIn">
+        <Typography fontWeight={700}>{product.title}</Typography>
+        <Typography fontWeight={500}>${product.price}</Typography>
+      </Box>
     </Grid>
   );
 };
