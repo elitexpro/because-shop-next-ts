427e97e7a4dc53b7b48049e5ea82b250c1a9b5a7
elitexpro <cryptooxelite@gmail.com>
2023-03-13 07:06:42 -0500

    [ADD]: initial MUI theme configuration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,15 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
-import '@/styles/globals.css'
+import { lightTheme } from '@/themes';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
