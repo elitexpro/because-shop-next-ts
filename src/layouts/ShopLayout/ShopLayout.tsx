c4e56a0523badfb9b199f06d62025bfc1b45d330
elitexpro <cryptooxelite@gmail.com>
2023-03-13 07:24:59 -0500

    [ADD]: shop layout init design

---
+++ src/layouts/ShopLayout/ShopLayout.tsx
@@ -0,0 +1,51 @@
+import Head from 'next/head';
+
+export interface ShopLayoutProps {
+  title: string;
+  pageDescription: string;
+  imageFullUrl?: string;
+
+  children: React.ReactNode;
+}
+
+const ShopLayout: React.FC<ShopLayoutProps> = ({
+  children,
+  title,
+  pageDescription,
+  imageFullUrl,
+}) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+
+        <meta name="description" content={pageDescription} />
+
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+        <meta name="og:title" content={title} />
+        <meta name="og:description" content={pageDescription} />
+        {imageFullUrl && <meta name="og:image" content={imageFullUrl} />}
+
+        <meta name="keywords" content="openjira, tasks, todos, manage" />
+      </Head>
+
+      <nav>{/* Navbar */}</nav>
+
+      {/* Sidebar */}
+
+      <main
+        style={{ margin: '80px auto', maxWidth: '1440px', padding: '0 30px' }}
+      >
+        {children}
+      </main>
+
+      <footer>
+        {/*  */}
+        {/*  */}
+      </footer>
+    </>
+  );
+};
+
+export default ShopLayout;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,18 @@
+import { ShopLayout } from '@/layouts';
+import { Typography } from '@mui/material';
+
 export default function HomePage() {
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <ShopLayout
+      title="Teslo-Shop | Home"
+      pageDescription="Find Teslo's best products here"
+    >
+      <Typography variant="h1" component="h1">
+        Shop
+      </Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        All products
+      </Typography>
+    </ShopLayout>
   );
 }
