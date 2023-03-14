1f9f5782fdcded1e96f7d2a2366fff7e11ae20c4
elitexpro <cryptooxelite@gmail.com>
2023-03-14 08:31:47 -0500

    [ADD]: Product model

---
+++ src/api/db/index.ts
@@ -0,0 +1,3 @@
+export { default as ProductModel } from './models/Product';
+
+// export { default as EntryModel, type IEntryModel } from './models/Entry';
---
+++ src/api/db/models/Product.ts
@@ -0,0 +1,63 @@
+import { IProduct } from '@/interfaces';
+import mongoose, { Schema, model, Model } from 'mongoose';
+
+const ProductSchema = new Schema(
+  {
+    title: { type: String, required: [true, 'Title is required!'], trim: true },
+    description: {
+      type: String,
+      required: [true, 'Description is required!'],
+      trim: true,
+    },
+    images: [{ type: String }],
+    inStock: {
+      type: Number,
+      required: [true, 'In Stock is required!'],
+      default: 0,
+    },
+    price: { type: Number, required: [true, 'Price is required!'], default: 0 },
+
+    // todo: relations - Aggregate Functions
+    sizes: [
+      {
+        type: String,
+        enum: {
+          values: ['XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL'],
+          message: '{VALUE} is not a valid size!',
+        },
+      },
+    ],
+
+    slug: {
+      type: String,
+      required: [true, 'Slug is required!'],
+      unique: true,
+      trim: true,
+    },
+    tags: [{ type: String }],
+
+    // todo: relations - Aggregate Functions
+    type: {
+      type: String,
+      enum: {
+        values: ['shirts', 'pants', 'hoodies', 'hats'],
+        message: '{VALUE} is not a valid type!',
+      },
+    },
+    gender: {
+      type: String,
+      enum: {
+        values: ['men', 'women', 'kid', 'unisex'],
+        message: '{VALUE} is not a valid gender!',
+      },
+    },
+  },
+
+  { timestamps: true, versionKey: false }
+);
+
+// si ya existe el model usalo, sino crealo
+const ProductModel: Model<IProduct> =
+  mongoose.models.Product || model('Product', ProductSchema);
+
+export default ProductModel;
