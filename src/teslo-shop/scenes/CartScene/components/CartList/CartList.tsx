8ef4c8b1ff5996cda0d6830ff5ce50d4316f25f0
elitexpro <cryptooxelite@gmail.com>
2023-03-13 20:31:10 -0500

    [ADD]: cart list component

---
+++ src/teslo-shop/scenes/CartScene/components/CartList/CartList.tsx
@@ -0,0 +1,71 @@
+import NextLink from 'next/link';
+import {
+  Box,
+  Button,
+  CardActionArea,
+  CardMedia,
+  Grid,
+  Typography,
+} from '@mui/material';
+
+import { initialData } from '@/database/products';
+import { ItemCounter } from '@/shared/components';
+
+interface CartListProps {}
+
+const productsInCart = [
+  initialData.products[0],
+  initialData.products[1],
+  initialData.products[2],
+];
+
+const CartList: React.FC<CartListProps> = () => {
+  return (
+    <>
+      {productsInCart.map(product => (
+        <Grid key={product.slug} container spacing={2} sx={{ mb: 1 }}>
+          <Grid item xs={3}>
+            <NextLink href={`/product/slug`}>
+              <CardActionArea>
+                <CardMedia
+                  image={`products/${product.images[0]}`}
+                  component="img"
+                  sx={{ borderRadius: '5px' }}
+                ></CardMedia>
+              </CardActionArea>
+            </NextLink>
+          </Grid>
+
+          <Grid item xs={7}>
+            <Box display="flex" flexDirection="column">
+              <Typography variant="body1">{product.title}</Typography>
+              <Typography variant="body1">
+                Size: <strong>M</strong>
+              </Typography>
+
+              {/* conditional */}
+              <ItemCounter />
+            </Box>
+          </Grid>
+
+          <Grid
+            item
+            xs={2}
+            display="flex"
+            alignItems="center"
+            flexDirection="column"
+          >
+            <Typography variant="subtitle1">${product.price}</Typography>
+
+            {/* editable */}
+            <Button variant="text" color="secondary">
+              Remove
+            </Button>
+          </Grid>
+        </Grid>
+      ))}
+    </>
+  );
+};
+
+export default CartList;
